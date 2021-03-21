874d66da8a9eed5e9d35633ae7eb1a4441551ed1
Kerdoudi <55836@etu.he2b.be>
2021-03-21 12:09:50 +0100

    Class tile début du projet les commentaires se feront par la suite

---
+++ src/g55836/luckynumbers/tile/Tile.java
@@ -0,0 +1,13 @@
+package g55836.luckynumbers.tile;
+
+public class Tile
+{
+    private int value;
+
+    public Tile(int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue() { return value; }
+}
