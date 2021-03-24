ab7cf206289146c4280a1452347b7572a764c961
Kerdoudi <55836@etu.he2b.be>
2021-03-24 22:13:09 +0100

    Board + BoardTest

---
+++ src/g55836/luckynumbers/controller/Controller.java
@@ -0,0 +1,36 @@
+package g55836.luckynumbers.controller;
+
+import g55836.luckynumbers.model.Model;
+import g55836.luckynumbers.view.View;
+
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+    public void play() {
+        view.displayWelcome();
+        while (true) {
+            switch (model.getState()) {
+                case NOT_STARTED:
+                case GAME_OVER:
+                    view.displayWinner();
+                    model.start(view.askPlayerCount());
+                    break;
+                case PICK_TILE:
+                    model.pickTile();
+                    view.displayGame();
+                    break;
+                case PLACE_TILE:
+                    model.putTile(view.askPosition());
+                    break;
+                case TURN_END:
+                    model.nextPlayer();
+                    break;
+            }
+        }
+    }
+}
